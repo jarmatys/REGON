74216520747c8ae3835cd4d60cc6c011a2f8dab8
Jarek Armatys <jarekarmatys@gmail.com>
2023-08-02 16:46:15 +0200

    Usunięto niepotrzebny powershell skrypt

--- publish.ps1
+++
@@ -1,9 +0,0 @@
-param ($version)
-
-if ($version -eq $null) {
-    $version = read-host -Prompt "Please enter package version" 
-} 
-
-dotnet pack --configuration Release
-
-dotnet nuget push ./bin/Release/REGON.1.0.1.nupkg -s https://api.nuget.org/v3/index.json -k oy2n6v7om373ulpjz5zatwtnaseuymmujovfzwuwqilpr4
